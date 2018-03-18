cbd2c6f59a1bbb6ae79e7bc53354b995db1df0db
levkowalski <levkowalski@gmail.com>
2018-03-18 22:02:10 +0100

    add matterjs

--- src/Index.js
+++ src/Index.js
@@ -1,12 +1,30 @@
-import 'p5';
+import * as Matter from 'matter-js';
 
-window.setup = function () {
-  console.log('Setup');
-};
+// module aliases
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-window.draw = function () {
-  console.log('Draw');
-  // ellipse(50, 50, 80, 80);
-  createCanvas(640, 480);
-  background(0, 101, 202);
-};
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+  element: document.body,
+  engine: engine
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
